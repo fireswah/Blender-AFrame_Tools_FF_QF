0d0d359f699141b86e1be84f42af62b2f2046c2e
fireswah <52438028+fireswah@users.noreply.github.com>
2024-05-23 08:07:59 -0600

    Update BlenderFastFuels.py
    -Eliminate creating dem.csv from zarr, it reads directly with better use of zarr file info.
    - Trees and terrain matched consistently (so far in my testing)
    -Stores all treelist data on a per-vertex basis, which means you can now edit the points (delete trees off roads, etc) in Blender and the data is retained.
    -Separated exporting a new treelist.csv to a second script.  This is so you can edit the trees before writing the new treelist, and starts the process of modularizing for a Blender plugin.

--- BlenderFastFuels.py
+++ BlenderFastFuels.py
@@ -1,13 +1,13 @@
 """
 Author: Matt Gibson
-    #This Blender 4, Python 3 script is used with output already created from FastFuels.
+    #This Blender 4 Python 3 script is for use with a FastFuels zarr array
     #Python will need the zarr module installed (i.e.: pip -m install zarr).
         #If you have multiple versions of Python outside of Blender, renaming Blender's version
             #to a backup will default Blender to your main Python.  See:
             #https://blender.stackexchange.com/questions/5287/using-3rd-party-python-modules
-    #Extract the fuel-array.zip in the same folder as your Blender file.
+    #Put the fuel-array.zip into the same folder as your Blender file.
     #Put the treelist.csv into the same folder as well if doing trees.
-    #+Y axis should be north (but needs further verifying).
+    #Blender +Y axis should be north (but needs further verifying).
 """
 
 #IMPORT STATEMENTS
@@ -17,15 +17,12 @@ import bpy
 import pathlib
 import csv
 import zarr
-from math import pi
 
 """
 SETUP OPTIONS
 """
 #Clear current Collection in Blender
 clearCollection = True
-#Create the name of the file in quotes to name your output DEM csv.
-outputDEM = 'elDdem.csv'
 #DEM Resolution - Default from LANLGUI is 2M.  Don't change unless you changed in LANL.
 demRes = 2
 #'Quads' or 'Tris' for terrain generation
@@ -37,7 +34,7 @@ terrainName = 'el-dorado-ground'
 #Name of tree object created in Collection
 treesName = 'el-dorado-trees'
 #Name of the new treelist.csv to write
-newTreeFile = 'ElDoradoUnreal.csv'
+newTreeFile = 'ElDoradoExport.csv'
 
 """
 Variables, Blender Paths, References
@@ -45,40 +42,27 @@ Variables, Blender Paths, References
 file_loc = pathlib.Path( __file__ ).parent.resolve() #this blender file
 folder = pathlib.Path( file_loc ).parent.resolve() #folder file is in
 data_coll = bpy.data.collections[ 'Collection' ] #name of collection in Blender Scene Collection (upper right)
-
-csvdata = pathlib.Path( folder ).joinpath( outputDEM )
 treedata = pathlib.Path( folder ).joinpath( treefile )
 newtreedata = pathlib.Path( folder ).joinpath( newTreeFile )
 
-vertices = []
-edges = []
-faces = []
-treeverts = []
-treeedges = []
-treefaces = []
-heightMax = 0
+ny = 0
+nx = 0
+xmax = 0
+xmin = 0
+ymax = 0
+ymin = 0
+zmax = 0
+zmin = 0
+xRange = 0
+yRange = 0
+zRange = 0
+
+demVerts = []
+demFaces = []
+treeVerts = []
+treeEdges = []
+treeFaces = []
 allTreeData = []
-nx = 0 #This should match arraysize.txt output after running.  ElD = 437
-ny = 0 #This should match arraysize.txt output after running.  ElD = 396
-
-"""
-Remap number range function
-"""
-def num_to_range( num, inMin, inMax, outMin, outMax ):
-    return outMin + ( float( num - inMin ) / float(inMax - inMin ) * ( outMax - outMin ) )
-
-"""
-Creates .csv file of the elevation points from the zarr array.  Zarr only has elevation.
-"""
-def createElevCSV():
-    fpath = pathlib.Path( folder ).joinpath( 'fuel-array/surface' )
-    voxels = zarr.open( fpath )
-    zarr_array_dem = voxels[ 'DEM' ]
-    dem_data = np.array( zarr_array_dem )
-    with open( csvdata, 'w' ) as f:
-        np.savetxt( f, dem_data, fmt='%.4d', delimiter=",", newline='\n' )
-    #print( zarr_array_dem.info )
-    #print( str( "Array is " ) + str( len( dem_data ) ) + str( " items in length." ) )
 
 """
 In Blender, this will clear the Collection of objects in the scene.
@@ -89,33 +73,27 @@ def clearBlenderCollection():
             bpy.data.objects.remove( ob )
 
 """
-Terrain Polygon Generation
-Remember that in the data it is Y, X!
+Remap number range function
 """
-def generateDEMGrid(): 
-    global heightMax
-    global nx
-    global ny
-    with open( csvdata, 'r', newline='' ) as csvfile:
-        datareader = csv.reader( csvfile )
-        colRes = 0
-        ny = 0
-        for yValues in datareader:
-            rowRes = 0
-            nx = 0
-            for index, elev in enumerate( yValues ):
-                #get/set height for later use by trees.
-                if int( elev ) > heightMax:
-                   heightMax = int( elev )
-                point = [ int( rowRes ), int( colRes ), int( elev ) ]
-                vertices.append( point )
-                rowRes += demRes
-                nx += 1
-            colRes += demRes
-            ny += 1
-        #print( ny ) #Use these to confirm against arraysize.txt output
-        #print( nx )
-    #Create faces as quadrangles or triangles    
+def num_to_range( num, inMin, inMax, outMin, outMax ):
+    return outMin + ( float( num - inMin ) / float(inMax - inMin ) * ( outMax - outMin ) )
+
+"""
+Creates a Quad or Tri faced terrain object utilizing the zarr array only
+*Still need to address "bounds" from FF and test?
+"""
+def generateDEMGrid( ny, nx, xmin, ymin, heightarray ): 
+    y = ymin
+    print(ny, nx)
+    for a in range(ny):
+        x = xmin
+        for b in range(nx):
+            #Blender posX - left, poxY - towards viewer, posz - up
+            point = [ x, y, heightarray[ a ][ b ] ]
+            demVerts.append( point )
+            x = x + demRes
+        y = y + demRes
+    #Quads and Tris working, but I still need to clean them up better so they are less confusing!
     if polyGen == 'Quads':
         facerows = ny
         initialColumn = nx
@@ -124,7 +102,7 @@ def generateDEMGrid():
         for frows in range( 0, facerows - 1 ):
             for v in range( rangeV, column ):
                 poly = [ v, v + 1, v + initialColumn + 1, v + initialColumn ]
-                faces.append( poly )
+                demFaces.append( poly )
             rangeV = rangeV + initialColumn
             column = column + initialColumn
     elif polyGen == 'Tris':
@@ -136,17 +114,17 @@ def generateDEMGrid():
             for v in range( rangeV, column ):
                 tri1 = [ v, v + 1, v + initialColumn ]
                 tri2 = [ v + 1, v + initialColumn + 1, v + initialColumn ]
-                faces.append( tri1 )
-                faces.append( tri2 )
+                demFaces.append( tri1 )
+                demFaces.append( tri2 )
             rangeV = rangeV + initialColumn
             column = column + initialColumn
 
 """
-Blender terrain object creation
+Blender create ground in collection, set origin
 """
 def createGroundObject( name ):
     new_mesh = bpy.data.meshes.new( 'grounddata' )
-    new_mesh.from_pydata( vertices, edges, faces )
+    new_mesh.from_pydata( demVerts, [], demFaces )
     new_mesh.update()
 
     new_object = bpy.data.objects.new( name, new_mesh )
@@ -157,77 +135,105 @@ def createGroundObject( name ):
     bpy.ops.object.origin_set( type='GEOMETRY_ORIGIN', center='BOUNDS' )
 
 """
-Create trees and place on terrain
-"""    
+Create Tree Points from original .csv lat/lons.
+*The terrain matches the bounding box of trees already now.
+"""
 def createTreePoints():
     with open( treedata, 'r', newline='' ) as csvfile:
         datareader = csv.reader( csvfile, delimiter=',' )
-        next( csvfile )
-        #Initialize max/min for x and y
-        for init in datareader:
-            maxX = float( init[ 7 ] )
-            minX = float( init[ 7 ] )
-            maxY = float( init[ 8 ] )
-            minY = float( init[ 8 ] )
-            #print(maxX, minX, maxY, minY)
-            break
-        
-        #Reset reader and skip header row again
-        csvfile.seek( 0 )
-        next( csvfile )
-        
-        #Get max/min for x and y
-        for row in datareader:
-            if abs( float( row[ 7 ] ) ) > maxX:
-                maxX = float( row[ 7 ] )
-            if abs( float( row[ 7 ] ) ) < minX:
-                minX = float( row[ 7 ] )       
-            if abs( float( row[ 8 ] ) ) > maxY:
-                maxY = float( row[ 8 ] )
-            if abs( float( row[ 8 ] ) ) < minY:
-                minY = float( row[ 8 ] )
-            #print(maxX, minX, maxY, minY)
-
-        #Set Ranges
-        rangeX = float(maxX - minX)
-        rangeY = float(maxY - minY)
-
-        #Reset reader and skip header again
-        csvfile.seek(0)
-        next( csvfile )
-        
+        next( csvfile )#skip header row
         for tree in datareader:
             origX = float( tree [ 7 ] )
             origY = float( tree [ 8 ] )
-            newX = num_to_range( origX, minX, maxX, 0, rangeX )
-            newY = num_to_range( origY, minY, maxY, 0, rangeY )
-            treepoint = [ newX, newY, heightMax ]
-            treeverts.append( treepoint )
+            treepoint = [ origX, origY, 0 ]
+            treeVerts.append( treepoint )
             allTreeData.append( [ tree[0], tree[1], tree[2], tree[3], tree[4], tree[5], tree[6], tree[7], tree[8], 0, tree[9] ] )
-            #print(treeverts)
 
 """
-Create Terrain and Trees in Blender viewport and shrinkwrap trees to terrain
+Blender create trees in collection, add per vertex attribtue data set origin
 """
 def createTrees( name ):
+    #Create the trees as a mesh of points and add to scene
     new_mesh = bpy.data.meshes.new( 'treedata' )
-    new_mesh.from_pydata( treeverts, treeedges, treefaces )
+    new_mesh.from_pydata( treeVerts, treeEdges, treeFaces )
     new_mesh.update()
-
     new_object = bpy.data.objects.new( name, new_mesh )
     data_coll.objects.link( new_object )
 
+    #set original tree data as attributes on each vertex(data will be in model now, instead of just memory/csv)
+    ids = []
+    spcd = []
+    dia = []
+    ht = []
+    status = []
+    cbh = []
+    crownrad = []
+    xM = []
+    yM = []
+    bas = []
+
+    obj = bpy.context.scene.objects[ name ]
+    attrID = obj.data.attributes.new( name="ID", type="INT", domain="POINT" )
+    attrSPCD = obj.data.attributes.new( name="SPCD", type="INT", domain="POINT" )
+    attrDIA = obj.data.attributes.new( name="DIA_cm", type="FLOAT", domain="POINT" )
+    attrHT = obj.data.attributes.new( name="HT_m", type="FLOAT", domain="POINT" )
+    attrSTATUS = obj.data.attributes.new( name="STATUSCD", type="INT", domain="POINT" )
+    attrCBH = obj.data.attributes.new( name="CBH_m", type="FLOAT", domain="POINT" )
+    attrCROWNR = obj.data.attributes.new( name="CROWN_RADIUS_m", type="FLOAT", domain="POINT" )
+    attrX = obj.data.attributes.new( name="X_m", type="FLOAT", domain="POINT" )
+    attrY = obj.data.attributes.new( name="Y_m", type="FLOAT", domain="POINT" )
+    attrBAS = obj.data.attributes.new( name="basal_tree_ft^2", type="FLOAT", domain="POINT" )
+    for i in range( len( obj.data.vertices ) ):
+        ids.append( int( allTreeData[ i ][ 0 ] ) )
+        spcd.append( int( allTreeData[ i ][ 1 ] ) )
+        if allTreeData[ i ][ 2 ] == '':
+            dia.append( float( 0 ) )
+        else:
+            dia.append( float( allTreeData[ i ][ 2 ] ) )
+        if allTreeData[ i ][ 3 ] == '':
+            ht.append( float( 0 ) )
+        else:
+            ht.append( float( allTreeData[ i ][ 3 ] ) )
+        if allTreeData[ i ][ 4 ] == '':
+            status.append( int( 0 ) )
+        else:
+            status.append( int( allTreeData[ i ][ 4 ] ) )
+        if allTreeData[ i ][ 5 ] == '':
+            cbh.append( float( 0 ) )
+        else:
+            cbh.append( float( allTreeData[ i ][ 5 ] ) )
+        if allTreeData[ i ][ 6 ] == '':
+            crownrad.append( float( 0 ) )
+        else:
+            crownrad.append( float( allTreeData[ i ][ 6 ] ) )
+        xM.append( float( allTreeData[ i ][ 7 ] ) )
+        yM.append( float( allTreeData[ i ][ 8 ] ) )
+        if allTreeData[ i ][ 9 ] == '':
+            bas.append( float( 0 ) )
+        else:
+            bas.append( float( allTreeData[ i ][ 9 ] ) )
+    obj.data.attributes[ 'ID' ].data.foreach_set( "value", ids )
+    obj.data.attributes[ 'SPCD' ].data.foreach_set( "value", spcd )
+    obj.data.attributes[ 'DIA_cm' ].data.foreach_set( "value", dia )
+    obj.data.attributes[ 'HT_m' ].data.foreach_set( "value", ht )
+    obj.data.attributes[ 'STATUSCD' ].data.foreach_set( "value", status )
+    obj.data.attributes[ 'CBH_m' ].data.foreach_set( "value", cbh )
+    obj.data.attributes[ 'CROWN_RADIUS_m' ].data.foreach_set( "value", crownrad )
+    obj.data.attributes[ 'X_m' ].data.foreach_set( "value", xM )
+    obj.data.attributes[ 'Y_m' ].data.foreach_set( "value", yM )
+    obj.data.attributes[ 'basal_tree_ft^2' ].data.foreach_set( "value", bas )
+
+    #Move tree points to origin from original lat/lon and set above terrain for next step.
     bpy.data.objects[ name ].select_set( True )
     bpy.context.view_layer.objects.active = bpy.data.objects[ name ]
     bpy.ops.object.origin_set( type='GEOMETRY_ORIGIN', center='BOUNDS' )
-    bpy.ops.object.location = (0, 0, heightMax )
+    bpy.ops.object.location = (0, 0, 2000 )#set high so it's above highest terrain point
 
 """
 treesToTerrain is Blender specific.  It adds a "shrinkwrap modifier" to the points on a negative Z axis
 and moving them to where that intersects with the target (terrain).  Applying this modifier will automatically
 change the vertex locations so we can get them back at elevation.
 """
-
 def treesToTerrain( treeObject, target ):
     C = bpy.context
     obj = C.scene.objects[ treeObject ]
@@ -242,46 +248,32 @@ def treesToTerrain( treeObject, target ):
     bpy.ops.object.modifier_apply( modifier = 'wrap' )
 
 """
-Write new treelist with elevations included.
+Zarr info and data set variables
 """
-def writeNewTreeList():
-    newverts = []
-    bpy.data.objects[ treesName ].select_set(True)
-    obj = bpy.data.objects[ treesName ]
-    for vert in obj.data.vertices:
-        x, y, z = vert.co
-        newverts.append( [x, y, z] )
-    #print( len(vertZ) )
-    with open( newtreedata, 'w', newline='' ) as newf:
-        writer = csv.writer( newf )
-        #Add header row to all tree data
-        writer.writerow( [ 'ID','SPCD','DIA_cm','HT_m','STATUSCD','CBH_m','CROWN_RADIUS_m','X_m','Y_m','Z_m','basal_tree_ft^2' ] )
-        #Write new csv using the new adjusted vert coordinates
-        for atd, newv in zip( allTreeData, newverts):
-            id = atd[0]
-            spcd = atd[1]
-            dia = atd[2]
-            ht = atd[3]
-            stat = atd[4]
-            cbh = atd[5]
-            crad = atd[6]
-            #skip to 10.  7 and 8 were orig. x/y and 9 I added
-            #in above as placeholder z in createTreePoints()
-            bas = atd[10]
-            newx = newv[0]
-            newy = newv[1]
-            newz = newv[2]
-            writer.writerow( [ id, spcd, dia, ht, stat, cbh, crad, newx, newy, newz, bas ] )
-    print( "Writing " + newTreeFile )
+fpath = pathlib.Path( folder ).joinpath( 'fuel-array.zip' )
+zarr_root = zarr.open( fpath )
+attrs = zarr_root.attrs
+ny = attrs[ 'ny' ]
+nx = attrs[ 'nx' ]
+xmax = attrs[ 'xmax' ]
+xmin = attrs[ 'xmin' ]
+ymax = attrs[ 'ymax' ]
+ymin = attrs[ 'ymin' ]
+xRange = xmax - xmin
+yRange = ymax - ymin
+surface = zarr_root.surface
+dem = surface[ 'DEM' ]
+npheightarray = dem[...]
+zmax = np.max( npheightarray )
+zmin = np.min( npheightarray )
+zRange = zmax - zmin
 
 """
 Run it all in the order I built it.
 """
-createElevCSV()
 clearBlenderCollection()
-generateDEMGrid()
+generateDEMGrid( int(ny), int(nx), int(xmin), int(ymin), npheightarray )
 createGroundObject( terrainName )
 createTreePoints()
 createTrees( treesName )
 treesToTerrain( treesName, terrainName )
-writeNewTreeList()
